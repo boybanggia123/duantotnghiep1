70dee751ed0933fab859315c02efeafca9fd988e
boybanggia123 <quanbui0999@gmail.com>
2024-10-07 08:47:32 +0700

    cập nhật lại back-end

--- back-end/model/db.js
+++ back-end/model/db.js
@@ -1,12 +1,22 @@
 const { MongoClient } = require("mongodb");
 
 const url = "mongodb://localhost:27017";
-const dbName = "tên mongodb mình tạo";
+const dbName = "duantotnghiep";
 
 async function connectDb() {
-  const client = new MongoClient(url);
-  await client.connect();
-  console.log("Kết nối thành công đến server");
-  return client.db(dbName);
+  const client = new MongoClient(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+
+  try {
+    await client.connect();
+    console.log("Kết nối thành công đến server");
+    return client.db(dbName);
+  } catch (error) {
+    console.error("Lỗi kết nối đến server:", error);
+    throw error;
+  }
 }
+
 module.exports = connectDb;
