660fcd064245e663e53e79eedb608df676abc900
datnguyen <dattannguyentan04@gmail.com>
2024-11-16 13:36:26 +0700

    update favourite

--- front-end/src/app/chitiet/[id]/page.jsx
+++ front-end/src/app/chitiet/[id]/page.jsx
@@ -85,6 +85,12 @@ export default function Detail({ params }) {
       <div aria-label="breadcrumb">
         <ol className="breadcrumb">
           <li className="Detail">
+            <Link href="/">Home</Link>
+          </li>
+          <li className="item_detail">
+            <i className="fa-solid fa-chevron-right"></i>
+          </li>
+          <li className="Detail">
             <Link href="#">Chi tiết</Link>
           </li>
           <li className="item_detail">
@@ -127,6 +133,15 @@ export default function Detail({ params }) {
 
         <div className="col-md-4">
           <div className="name_detail">{product.name}</div>
+          {/* <span className="icon-favourite" onClick={() => toggleFavourite(product)}>
+            <svg
+              className={`icon-svg ${isFavourite ? 'active' : ''}`}  // Thêm lớp 'active' nếu sản phẩm được yêu thích
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+            >
+              <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41 0.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3c2.08 0 4.5 2.42 4.5 5.5 0 3.78-3.4 6.86-8.55 11.54L12 21.35z" />
+            </svg>
+          </span> */}
           <p className="price_giam mb-2">
             <div className="gia_detail">
               {product.discountedPrice > 0 ? (
